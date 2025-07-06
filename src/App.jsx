6a404c5fb00c0a8d456ca91a221feb59ddd13ea4
bfftalk143 <bfftalk143@gmail.com>
2025-07-06 14:42:15 +0000

    vercel json added for header links

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,32 @@ import Privacy from "./pages/privacy";
 import Contact from "./pages/contact";
 import Header from "./components/Header";
 import SubdomainPage from "./pages/subdomains";
-import { getSubdomain } from "./pages/utils/getSubdomain"; // ✅ correct path
+import { getSubdomain } from "./pages/utils/getSubdomain";
 
 function AppContent() {
   const subdomain = getSubdomain();
+
+  if (subdomain) {
+    return (
+      <div className="flex min-h-screen flex-col">
+        <Header />
+        <main className="flex-1">
+          <SubdomainPage subdomain={subdomain} />
+        </main>
+      </div>
+    );
+  }
 
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <main className="flex-1">
-        {subdomain ? (
-          <SubdomainPage subdomain={subdomain} />
-        ) : (
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/privacy" element={<Privacy />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        )}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </main>
     </div>
   );
